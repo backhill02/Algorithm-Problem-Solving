5269865fc3409cd7cf0293896afee4abb772fb19
DG <backhill0202@gmail.com>
2017-04-13 15:16:38 +0900

    Add Cryptography - TopCoder (java)

---
+++ topcoder/java/src/main/java/Cryptography.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Cryptography {
+    public long encrypt(int[] numbers) {
+        long ret = 1;
+        Arrays.sort(numbers);
+        numbers[0]++;
+
+        for(int i=0; i<numbers.length; i++) {
+            ret *= numbers[i];
+        }
+        return ret;
+    }
+}
