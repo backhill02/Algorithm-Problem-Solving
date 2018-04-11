75878b00bb17eeeb133b83075e2d1654300946f4
DG <backhill0202@gmail.com>
2018-04-11 17:23:53 +0900

    Update InterestingDigits

--- topcoder/src/main/java/InterestingDigits.java
+++ topcoder/src/main/java/InterestingDigits.java
@@ -8,26 +8,7 @@ public class InterestingDigits {
     List<Integer> results = new ArrayList<>();
 
     for (int i = 2; i < base; i++) {
-      boolean isOk = true;
-
-      for (int k1 = 0; k1 < base; k1++) {
-        for (int k2 = 0; k2 < base; k2++) {
-          for (int k3 = 0; k3 < base; k3++) {
-            if ((k1 + k2 * base + k3 * base * base) % i == 0 && (k1 + k2 + k3) % i != 0) {
-              isOk = false;
-              break;
-            }
-          }
-          if (!isOk) {
-            break;
-          }
-        }
-        if (!isOk) {
-          break;
-        }
-      }
-
-      if (isOk) {
+      if ((base - 1) % i == 0) {
         results.add(i);
       }
     }
