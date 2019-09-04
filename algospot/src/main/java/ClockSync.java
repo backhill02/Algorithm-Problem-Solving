380c657b1bb373201fba7ad23ec252f17b2a5c36
DG <backhill0202@gmail.com>
2019-09-05 02:13:15 +0900

    Add CLOCKSYNC (https://algospot.com/judge/problem/read/CLOCKSYNC)

--- algospot/src/main/java/ClockSync.java
+++ algospot/src/main/java/ClockSync.java
@@ -1,4 +1,7 @@
-import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class ClockSync {
     private static final int[][] CONNECTED_CLOCK = {
@@ -13,29 +16,52 @@ public class ClockSync {
             {1, 2, 3, 4, 5},
             {3, 4, 5, 9, 13}
     };
+    private static int[] clocks = new int[16];
+    private static final int MIN = 9999;
 
-    public static void main(String[] args) {
-        int[] clocks = {12, 6, 6, 6, 6, 6, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12};
-        press(0, new Stack<>(), clocks);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCnt = Integer.parseInt(br.readLine().trim());
+        StringBuilder result = new StringBuilder();
+        for (int t = 0; t < testCnt; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int c = 0; c < clocks.length; c++) {
+                clocks[c] = Integer.parseInt(st.nextToken());
+            }
+            int solve = solve(0);
+            result.append(solve >= MIN ? -1 : solve).append("\n");
+        }
+        System.out.print(result);
     }
 
-    private static void press(int ccIndex, Stack<Integer> ccStack, int[] clocks) {
-        if (ccIndex == 10) {
-            return;
+    private static boolean areAligned() {
+        boolean isOk = true;
+        for (int clock : clocks) {
+            if (clock != 12) {
+                isOk = false;
+                break;
+            }
         }
-
-        for (int i = ccIndex; i < 10; i++) {
-            changeTime(clocks, ccStack.push(i), 3);
-            press(i + 1, ccStack, clocks);
-            changeTime(clocks, ccStack.pop(), -3);
-        }
+        return isOk;
     }
 
-    private static void changeTime(int[] clocks, int ccIndex, int addHour) {
-        for (int cIndex : CONNECTED_CLOCK[ccIndex]) {
-            int change = (clocks[cIndex] + addHour) % 12;
-            change = change == 0 ? 12 : change;
-            clocks[cIndex] = change;
+    private static int solve(int switchIndex) {
+        if (switchIndex == 10) {
+            return areAligned() ? 0 : MIN;
+        }
+
+        int ret = MIN;
+        for (int cnt = 0; cnt < 4; cnt++) {
+            ret = Math.min(ret, cnt + solve(switchIndex + 1));
+            press(switchIndex);
+        }
+        return ret;
+    }
+
+    private static void press(int switchIndex) {
+        for (int cIndex : CONNECTED_CLOCK[switchIndex]) {
+            clocks[cIndex] += 3;
+            if (clocks[cIndex] >= 15) clocks[cIndex] = 3;
         }
     }
 }
