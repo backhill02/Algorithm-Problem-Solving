1eec8aed72fc7e588e6637fa38ebe3605390ef72
DG <backhill0202@gmail.com>
2019-10-23 08:18:58 +0900

    Add Problem2748 (https://www.acmicpc.net/problem/2748) 피보나치 수 2 (Java)

---
+++ beakjoon/src/main/java/Problem2748.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Problem2748 {
+
+    private static long[] cache = new long[91];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Arrays.fill(cache, -1L);
+        System.out.print(solve(Integer.parseInt(br.readLine())));
+    }
+
+    private static long solve(int n) {
+        if (cache[n] != -1) return cache[n];
+        if (n < 2) {
+            return cache[n] = n;
+        } else {
+            return cache[n] = solve(n - 2) + solve(n - 1);
+        }
+    }
+}
