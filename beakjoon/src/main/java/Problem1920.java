8a50f17eaa96ee6ff346bfc6588253dd8655810a
dgseo <backhill0202@gmail.com>
2020-02-17 11:10:06 +0900

    Add Problem1920.java (https://www.acmicpc.net/problem/1920)

---
+++ beakjoon/src/main/java/Problem1920.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Problem1920 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        Set<Integer> a = Arrays.stream(br.readLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toSet());
+        br.readLine();
+        List<Integer> b = Arrays.stream(br.readLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        solve(a, b);
+    }
+
+    public static void solve(Set<Integer> a, List<Integer> b) {
+        for (int i : b) {
+            System.out.println(a.contains(i) ? 1 : 0);
+        }
+    }
+}
