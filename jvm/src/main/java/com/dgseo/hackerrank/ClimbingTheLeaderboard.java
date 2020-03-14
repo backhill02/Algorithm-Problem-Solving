876486f0f20f0e4992c3747fb14961b5d0c07a77
dgseo <backhill0202@gmail.com>
2020-03-14 18:33:40 +0900

    Improvement ClimbingTheLeaderboard.java (https://www.hackerrank.com/challenges/climbing-the-leaderboard/problem)

--- jvm/src/main/java/com/dgseo/hackerrank/ClimbingTheLeaderboard.java
+++ jvm/src/main/java/com/dgseo/hackerrank/ClimbingTheLeaderboard.java
@@ -5,6 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ClimbingTheLeaderboard {
+    public static void main(String[] args) {
+        int[] scores = {295, 294, 291, 287, 287, 285, 285, 284, 283, 279, 277, 274, 274, 271, 270, 268, 268, 268, 264, 260, 259, 258, 257, 255, 252, 250, 244, 241, 240, 237, 236, 236, 231, 227, 227, 227, 226, 225, 224, 223, 216, 212, 200, 197, 196, 194, 193, 189, 188, 187, 183, 182, 178, 177, 173, 171, 169, 165, 143, 140, 137, 135, 133, 130, 130, 130, 128, 127, 122, 120, 116, 114, 113, 109, 106, 103, 99, 92, 85, 81, 69, 68, 63, 63, 63, 61, 57, 51, 47, 46, 38, 30, 28, 25, 22, 15, 14, 12, 6, 4};
+        int[] alice = {5, 5, 6, 14, 19, 20, 23, 25, 29, 29, 30, 30, 32, 37, 38, 38, 38, 41, 41, 44, 45, 45, 47, 59, 59, 62, 63, 65, 67, 69, 70, 72, 72, 76, 79, 82, 83, 90, 91, 92, 93, 98, 98, 100, 100, 102, 103, 105, 106, 107, 109, 112, 115, 118, 118, 121, 122, 122, 123, 125, 125, 125, 127, 128, 131, 131, 133, 134, 139, 140, 141, 143, 144, 144, 144, 144, 147, 150, 152, 155, 156, 160, 164, 164, 165, 165, 166, 168, 169, 170, 171, 172, 173, 174, 174, 180, 184, 187, 187, 188, 194, 197, 197, 197, 198, 201, 202, 202, 207, 208, 211, 212, 212, 214, 217, 219, 219, 220, 220, 223, 225, 227, 228, 229, 229, 233, 235, 235, 236, 242, 242, 245, 246, 252, 253, 253, 257, 257, 260, 261, 266, 266, 268, 269, 271, 271, 275, 276, 281, 282, 283, 284, 285, 287, 289, 289, 295, 296, 298, 300, 300, 301, 304, 306, 308, 309, 310, 316, 318, 318, 324, 326, 329, 329, 329, 330, 330, 332, 337, 337, 341, 341, 349, 351, 351, 354, 356, 357, 366, 369, 377, 379, 380, 382, 391, 391, 394, 396, 396, 400};
+
+        Arrays.stream(climbingLeaderboard(scores, alice)).forEach(System.out::println);
+    }
+
     private static int[] climbingLeaderboard(int[] scores, int[] alice) {
         List<Integer> ranks = Arrays.stream(scores)
                 .boxed()
@@ -14,7 +21,7 @@ public class ClimbingTheLeaderboard {
         int[] ret = new int[alice.length];
         for (int i = 0; i < alice.length; i++) {
             int aliceScore = alice[i];
-            if (aliceScore >= ranks.get(0)) {
+            if (aliceScore > ranks.get(0)) {
                 ret[i] = 1;
             } else if (aliceScore < ranks.get(ranks.size() - 1)) {
                 ret[i] = ranks.size() + 1;
@@ -27,15 +34,16 @@ public class ClimbingTheLeaderboard {
 
     private static int getRank(List<Integer> ranks, int l, int r, int score) {
         if (r - l <= 1) {
-            return r + 1;
+            if (ranks.get(l) == score) return l + 1;
+            else return r + 1;
         }
         int mid = (l + r) / 2;
         if (ranks.get(l) >= score && score >= ranks.get(mid)) {
             return getRank(ranks, l, mid, score);
-        } else if (ranks.get(mid) >= score && score >= ranks.get(r)) {
-            return getRank(ranks, mid, r, score);
+        } else if (ranks.get(mid + 1) >= score && score >= ranks.get(r)) {
+            return getRank(ranks, mid + 1, r, score);
         } else {
-            return -1;
+            return mid + 2;
         }
     }
 }
